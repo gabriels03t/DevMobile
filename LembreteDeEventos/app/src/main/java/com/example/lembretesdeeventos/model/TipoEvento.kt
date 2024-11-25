48a7efc7a1c513d63d61135624504de44716da77
gabriels03t <gabrielsouzaturecki@gmail.com>
2024-11-24 22:09:54 -0300

    último commit

--- LembreteDeEventos/app/src/main/java/com/example/lembretesdeeventos/model/TipoEvento.kt
+++
@@ -1,9 +0,0 @@
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "tipos_evento")
-data class TipoEvento(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val nome: String,
-    val descricao: String?
-)
