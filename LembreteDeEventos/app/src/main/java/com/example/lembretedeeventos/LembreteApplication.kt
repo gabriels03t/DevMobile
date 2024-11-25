c16c7ff759852d7ad66523c590e30f006d7554fc
gabriels03t <gabrielsouzaturecki@gmail.com>
2024-11-24 22:51:17 -0300

    Lembrete de Eventos

---
+++ LembreteDeEventos/app/src/main/java/com/example/lembretedeeventos/LembreteApplication.kt
@@ -0,0 +1,22 @@
+package  com.example.lembretedeeventos
+import android.app.Application
+import androidx.room.Room
+import com.example.lembretedeeventos.database.LembreteDatabase
+
+class LembreteApplication : Application() {
+
+    lateinit var database: LembreteDatabase
+        private set
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        database = Room.databaseBuilder(this, LembreteDatabase::class.java, "lembrete-db")
+            .build()
+    }
+
+    companion object {
+        lateinit var instance: LembreteApplication
+            private set
+    }
+}
---
+++ LembreteDeEventos/app/src/main/java/com/example/lembretedeeventos/dao/TipoEventoDao.kt
@@ -0,0 +1,22 @@
+package  com.example.lembretedeeventos.dao
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.lembretedeeventos.data.TipoEvento
+
+@Dao
+interface TipoEventoDao {
+
+    @Insert
+    suspend fun inserir(tipoEvento: TipoEvento)
+
+    @Update
+    suspend fun atualizar(tipoEvento: TipoEvento)
+
+    @Query("SELECT * FROM tipos_evento")
+    suspend fun listarTodos(): List<TipoEvento>
+
+    @Query("DELETE FROM tipos_evento WHERE id = :id")
+    suspend fun deletar(id: Int)
+}
